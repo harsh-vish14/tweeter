f7cab81f557a294424e492d14cc4e386109826ed
harsh-vish14 <harshkv9322@gmail.com>
2021-06-12 15:48:54 +0530

    env variable updated

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  env: {
+    AUTHKEY: process.env.AUTHKEY,
+    SERVER_AUTHKEY: process.env.SERVER_AUTHKEY,
+    DB_APIKEY: process.env.DB_APIKEY,
+    DB_AUTHDOMAIN: process.env.DB_AUTHDOMAIN,
+    DB_PROJECTID: process.env.DB_PROJECTID,
+    DB_STROAGE_BUCKET: process.env.DB_STROAGE_BUCKET,
+    DB_MESSAGINGSENDER_ID: process.env.DB_MESSAGINGSENDER_ID,
+    DB_APPID: process.env.DB_APPID,
+  },
 };
